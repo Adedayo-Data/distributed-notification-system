814182a0b395bbbf7f59599637e0bbeda4b1b105
Adedayo-Data <theophilusdayo@outlook.com>
2025-11-10 16:47:37 +0100

    FEAT: add Redis status updates and fix connection port

--- push-service/src/main/java/com/hng/pushservice/services/PushMessageServiceReceiver.java
+++ push-service/src/main/java/com/hng/pushservice/services/PushMessageServiceReceiver.java
@@ -1,16 +1,34 @@
 package com.hng.pushservice.services;
 
 import com.hng.pushservice.dto.PushMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RabbitListener(queues = "push.queue")
+@RequiredArgsConstructor
 public class PushMessageServiceReceiver {
+
+    private final StringRedisTemplate redisTemplate;
 
     @RabbitHandler
     public void consumePushQueue(PushMessage pushMessage){
+        // Message from RabbitMQ received - LOG.INFO
         System.out.println("push message received for MQ: " +pushMessage.getPushToken());
+
+        // Update Redis
+        String redisKey = "notification:" + pushMessage.getNotificationId();
+        redisTemplate.opsForValue().set(redisKey, "PROCESSING");
+
+    }
+
+    // Helper method to update Redis
+    private void updateStatus(String notifcationId, String status){
+        String key = "notification:"+ notifcationId;
+
+        redisTemplate.opsForValue().set(key, status);
     }
 }
